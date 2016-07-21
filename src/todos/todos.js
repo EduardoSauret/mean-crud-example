1f223a78ea7897a0a49fae45e45c42eb5bf56b16
eduardo sauret <eduardo.sauret@gmail.com>
2016-07-21 16:40:37 -0500

    create task update

--- src/todos/todos.js
+++ src/todos/todos.js
@@ -1,21 +1,37 @@
-export default function ($scope) {
-	$scope.todos = [
-	{
-		task: 'do dishes',
-		isCompleted: false
-	},
-	{
-		task: 'walk the dog',
-		isCompleted: true
-	}
-	];
+export default function($scope) {
+    let params = {
+        createHasInput: false
+    }
 
-	$scope.checkboxClick = todo => {
-		todo.isCompleted = !todo.isCompleted;
-		if (todo.isCompleted === true ) {
-			console.log('Checkbox selected ');
-		} else if (todo.isCompleted === false) {
-			console.log('Checkbox deselected ');
-		}
-	};
+    $scope.todos = [{
+        task: 'do dishes',
+        isCompleted: false
+    }, {
+        task: 'walk the dog',
+        isCompleted: true
+    }];
+
+    $scope.checkboxClick = todo => {
+        todo.isCompleted = !todo.isCompleted;
+        if (todo.isCompleted === true) {
+            console.log('Checkbox selected ');
+        } else if (todo.isCompleted === false) {
+            console.log('Checkbox deselected ');
+        }
+    };
+
+    $scope.$watch('createTaskInput', val => {
+        if (!val && params.createHasInput) {
+            $scope.todos.pop();
+            params.createHasInput = false;
+        } else if (val && !params.createHasInput) {
+            $scope.todos.push({ task: val, isCompleted: false });
+            params.createHasInput = true;
+        } else if (val && params.createHasInput) {
+        	$scope.todos[$scope.todos.lenght - 1].task = val;
+        }
+
+    });
+
+
 }
