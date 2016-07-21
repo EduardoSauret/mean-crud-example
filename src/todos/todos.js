326b7991bce717f31e3a0273fd89615178bb5504
eduardosauret <eduardo.sauret@gmail.com>
2016-07-21 01:17:53 -0500

    checkbox addclass

--- src/todos/todos.js
+++ src/todos/todos.js
@@ -9,4 +9,13 @@ export default function ($scope) {
 		isCompleted: true
 	}
 	];
+
+	$scope.checkboxClick = todo => {
+		todo.isCompleted = !todo.isCompleted;
+		if (todo.isCompleted === true ) {
+			console.log('Checkbox selected ');
+		} else if (todo.isCompleted === false) {
+			console.log('Checkbox deselected ');
+		}
+	};
 }
